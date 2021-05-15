583c2d752a3ed71d6e7d7c980eaa58c310bcc55c
bettyt12 <betitarekegn12@gmail.com>
2021-05-15 11:48:23 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,71 +1,78 @@
-
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import ReactStars from "react-rating-stars-component";
-import * as Statusbar from 'react-statusbar';
-import QuestionList  from './QuestionList.json';
-import { v4 as uuidv4} from "uuid";
+import * as Statusbar from "react-statusbar";
+import QuestionList from "./QuestionList.json";
 
 
 function App() {
+  const [currentquestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {}, [currentquestion]);
+
+  const handleCorrectAnswer = isCorrect => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    setClicked(true);
+  };
+  const handleNextQuestion = () => {
+    setClicked(false);
+    if (currentquestion < QuestionList.length - 1) {
+      setCurrentQuestion(currentquestion + 1);
+    }
+  };
 
 
-const [currentquestion,setCurrentQuestion]=useState(0);
-const [score,setScore]=useState(0);
-const [clicked,setClicked]=useState(false);
+  return (
+    <div className="content">
+      <div className="question_stat">
+        <h2>
+          Question {currentquestion + 1} of {QuestionList.length}
+        </h2>
+        <p> {QuestionList[currentquestion].category}</p>
+        <ReactStars />
+      </div>
+      <div className="question">
+        <p>{QuestionList[currentquestion].question}</p>
+      </div>
 
-useEffect(() => {
-    
-}, [currentquestion])
-   
-  const handleCorrectAnswer=(isCorrect)=>{
-      if (isCorrect){
-          setScore(score + 1);
-        }
-        setClicked(true);
-  }
-  const handleNextQuestion=()=>{
-        setClicked(false);
-        if(currentquestion < QuestionList.length-1){
-            setCurrentQuestion(currentquestion+1);
-        }
-  }
-  
-  console.log('check',QuestionList[currentquestion].incorrect_answers)
-       return(
-       <div className="content">
-        < div className="question_stat">
-            <h2>Question {currentquestion + 1} of {QuestionList.length}</h2>
-            <p> {QuestionList[currentquestion].category}</p>
-            <ReactStars/>
-        </div>
-        <div className="question">
-           <p>{QuestionList[currentquestion].question}</p>
-        </div>
-
-        <div className="btns">
-           {
-               console.log(QuestionList[currentquestion].incorrect_answers)
-               
-           }
-            {QuestionList[currentquestion].incorrect_answers.map((answerOption, index)=>(
-                     <button onClick={() => handleCorrectAnswer(answerOption.isCorrect)}
-                      key={index}>{answerOption}</button>   
-            ))}
-            {/* {QuestionList[currentquestion].correct_answer.map((answerOption)=>(
+      <div className="btns">
+        {/* {console.log(QuestionList[currentquestion].incorrect_answers)} */}
+        {QuestionList[currentquestion].incorrect_answers.map(
+          (answerOption, index) => (
+            <button
+              onClick={() => handleCorrectAnswer(answerOption.isCorrect)}
+              key={index}
+            >
+              {answerOption}
+            </button>
+          )
+        )}
+         {console.log(QuestionList[currentquestion].incorrect_answers)}
+        {QuestionList[currentquestion].correct_answer.map(
+          (answerOption, index) => (
+            <button
+              onClick={() => handleCorrectAnswer(answerOption.isCorrect)}
+              key={index}
+            >
+              {answerOption}
+            </button>
+          )
+        )}
+        {/* {QuestionList[currentquestion].correct_answer.map((answerOption)=>(
                  <button key={uuidv4()} onClick={handleCorrectAnswer(answerOption.isCorrect)}>
                      {answerOption.correct_answer}</button>
              ))} */}
 
-         </div>
-         <div >
-                <button onClick={() => handleNextQuestion()}>Next</button>
-         </div>
-       
-      
-        </div>
-       )
- 
+      </div>
+      <div>
+        <button onClick={() => handleNextQuestion()}>Next</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
